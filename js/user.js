838bab311ec1151172ddda04a2968e87880d109d
Elissa Sosland <esosland@gmail.com>
2016-05-27 20:05:28 -0700

    update getRepos method to print username

--- js/user.js
+++ js/user.js
@@ -3,10 +3,10 @@ var apiKey = require('./../.env').apiKey;
 exports.GithubUser = function(){
 }
 
-exports.GithubUser.prototype.getRepos = function() {
-  $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response) {
-    // $('.showUser').text("GitHub user: " + user + " Repositories: " + ?);
-    console.log(JSON.stringify(response));
+exports.GithubUser.prototype.getRepos = function(user) {
+  $.get('https://api.github.com/users/' + user + '?access_token=' + apiKey).then(function(response) {
+    $('.showUser').text("GitHub user: " + user + " Repositories: ");
+    // console.log(JSON.stringify(response));
   }).fail(function(error) {
     $('.showUser').text(error.responseJSON.message);
   });
