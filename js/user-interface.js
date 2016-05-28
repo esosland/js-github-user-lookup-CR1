4ea3be1bfdef056abe36232dd949a1d9b7c09ae2
Elissa Sosland <esosland@gmail.com>
2016-05-28 14:05:49 -0700

    add basic sass and empty method to clear results div

--- js/user-interface.js
+++ js/user-interface.js
@@ -4,6 +4,7 @@ var GithubObject = require ('./../js/user.js').GithubObject;
 $(document).ready(function() {
   var currentGithubObject = new GithubObject();
   $('#displayUsername').click(function() {
+    $('.showRepos').empty();
     var user = $('#username').val();
     $('#username').val("");
     currentGithubObject.getRepos(user);
