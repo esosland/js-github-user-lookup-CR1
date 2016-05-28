458825841191b0f2718a174b812ce1741e82cfba
Elissa Sosland <esosland@gmail.com>
2016-05-27 19:47:04 -0700

    add constructor and prototype function to user.js

--- js/user-interface.js
+++ js/user-interface.js
@@ -1,7 +1,11 @@
+var GithubUser = require ('./../js/user.js').GithubUser
 var getRepos = require('./../js/user.js').getRepos;
 
 $(document).ready(function() {
+  var currentGithubUser = new GithubUser();
   $('#displayUsername').click(function() {
-    getRepos();
+    var user = $('#username').val();
+    $('#username').val();
+    currentGithubUser.getRepos(user)
   });
 });
--- js/user.js
+++ js/user.js
@@ -1,21 +1,13 @@
 var apiKey = require('./../.env').apiKey;
 
-exports.getRepos = function(){
-  $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
-    console.log(response);
-  }).fail(function(error){
-    console.log(error.responseJSON.message);
-  });
-};
+exports.GithubUser = function(){
+}
 
-// exports.GithubUser = function(){
-// }
-//
-// exports.GithubUser.prototype.getGithubUser = function() {
-//   $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response) {
-//     // $('.showUser').text("GitHub user: " + user + " Repositories: " + ?);
-//     console.log(response);
-//   }).fail(function(error) {
-//     $('.showUser').text(error.responseJSON.message);
-//   });
-// }
+exports.GithubUser.prototype.getRepos = function() {
+  $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response) {
+    // $('.showUser').text("GitHub user: " + user + " Repositories: " + ?);
+    console.log(JSON.stringify(response));
+  }).fail(function(error) {
+    $('.showUser').text(error.responseJSON.message);
+  });
+}
